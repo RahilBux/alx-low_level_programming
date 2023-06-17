ff7fd978d0a70befb4640530a359accccbb0e1cb
RahilBux <rahilbux@gmail.com>
2023-06-17 14:36:30 -0700

    added task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 0-9
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+		while (num < 10)
+		{
+			printf("%d", num);
+				num++;
+		}
+	printf("\n");
+	return (0);
+}
