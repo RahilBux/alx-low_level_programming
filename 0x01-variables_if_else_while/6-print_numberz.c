71878e027762ff9b0452f014238304327deae5a3
RahilBux <rahilbux@gmail.com>
2023-06-17 14:52:40 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0-9 without printf
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	int num = 48;
+
+		while (num < 58)
+		{
+			putchar(num);
+				num++;
+		}
+		return (0);
+}
