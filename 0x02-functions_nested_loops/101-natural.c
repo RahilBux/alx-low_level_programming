d501ee9b5ebea9fbff0dc1df4778f869c4de2cfe
RahilBux <rahilbux@gmail.com>
2023-06-20 16:45:56 -0700

    update

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,9 +9,9 @@ int main(void)
 	int n;
 	int sum;
 
-	for (n = 0; n < 1024;n++)
+	for (n = 0; n < 1024; n++)
 	{
-		if((n % 3) == 0 || (n % 5) == 0)
+		if ((n % 3) == 0 || (n % 5) == 0)
 		{
 			sum += n;
 		}
