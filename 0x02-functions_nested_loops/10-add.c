542143c97e33d31d69b8845c440b4d0b59d61d5d
RahilBux <rahilbux@gmail.com>
2023-06-20 11:11:28 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers
+ * @x: value to be checked
+ * @y: value to be checked
+ * Return: int x + int y
+ */
+int add(int x, int y)
+{
+	int result;
+
+	result = int x + int y;
+	return (result);
+}
