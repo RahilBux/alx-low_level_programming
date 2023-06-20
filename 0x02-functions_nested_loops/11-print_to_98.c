1a76f6f95a2bbcbdaafb37feed558544af41c05e
RahilBux <rahilbux@gmail.com>
2023-06-20 14:48:14 -0700

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,22 +10,26 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		while (n >= 98)
+		for (; n >= 98; n--)
 		{
 			printf("%d", n);
-			putchar(',');
-			putchar(' ');
-			n--;
+			if (n != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	else if (n < 98)
 	{
-		while (n <= 98)
+		for (; n <= 98; n++)
 		{
 			printf("%d", n);
-			putchar(',');
-			putchar(' ');
-			n++;
+			if (n != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	else
