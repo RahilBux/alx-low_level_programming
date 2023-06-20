efff3cb0367f67c57c22e7425cd1aac2a86e7cff
RahilBux <rahilbux@gmail.com>
2023-06-20 16:22:13 -0700

    updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -37,7 +37,7 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar((c / 100)) + '0');
+					_putchar((c / 100) + '0');
 					_putchar(((c / 10) % 10) + '0');
 					_putchar((c % 10) + '0');
 				}
@@ -46,4 +46,3 @@ void print_times_table(int n)
 		}
 	}
 }
-
