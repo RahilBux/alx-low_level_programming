3c95a6a451a93a4effe84f772ba03f8ea9205ac4
RahilBux <rahilbux@gmail.com>
2023-06-26 12:24:11 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character
+ * @str: string input
+ */
+
+void puts2(char *str)
+{
+	int a = 0;
+	int b = 0;
+	char *c = str;
+	int d;
+
+	while (*c != '\0')
+	{
+		c++;
+		a++
+	}
+	b = a - 1;
+	for (d = 0; d <= b; d++)
+	{
+		if (d % 2 == 0)
+		{
+			_putchar(str[d]);
+		}
+	}
+	_putchar('\n');
+}
