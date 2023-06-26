3f9d3d0739d7b45d96f342353e275960c9de03cd
RahilBux <rahilbux@gmail.com>
2023-06-26 06:50:22 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98(int *n) - takes a pointer and update the value it points to to 98
+ * @*n: value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+README for 0x05-pointers_arrays_strings
+
