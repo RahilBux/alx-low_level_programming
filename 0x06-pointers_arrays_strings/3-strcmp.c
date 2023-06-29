c104b7724f83d9713ac12b1114c80994ee37cfbd
RahilBux <rahilbux@gmail.com>
2023-06-29 12:59:00 -0700

    task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strcmp - compares two strings lexicographically
+ * _strcmp - compares two strings lexicographically
  * @s1: string 1
  * @s2: string 2
  * Return: 0(Success)
@@ -9,7 +9,6 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
 
 	while (*s1 != '\0' && *s2 != '\0')
 	{
