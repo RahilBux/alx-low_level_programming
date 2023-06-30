1a1728fd663f2ee3b9762db3f8e9d05e125ce88c
RahilBux <rahilbux@gmail.com>
2023-06-30 14:04:12 -0700

    task 101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - prints number in parameter
+ * @n: integer parameter
+ */
+
+void print_number(int n)
+{
+	unsigned int i;
+
+	i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -n;
+	}
+	if (i / 10 != 0)
+	{
+		print_number(i / 10);
+	}
+	_putchar((i % 10) + '0');
+}
