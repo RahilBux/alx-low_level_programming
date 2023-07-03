5fa87ba19f3ab66db64adf9893d2e108e93ae79b
RahilBux <rahilbux@gmail.com>
2023-07-03 19:20:27 +0200

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints a chessboard
+ * *a: string of chessboard
+ */
+
+void print_chessboard(char(*a)[8])
+{
+	int i;
+	int n;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; n++)
+		{
+			_putchar(a[i][n]);
+		}
+		_putchar('\n');
+	}
+}
