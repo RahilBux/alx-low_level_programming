3557fabd21be65846fc530cab8bfb98ec467aaec
RahilBux <rahilbux@gmail.com>
2023-07-03 16:42:54 +0200

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string and then returns value from that character
+ * @s: pointer of the string
+ * @c: character to be located in the string
+ * Return: modified string
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+	if (s[i] == '\0')
+	{
+		return NULL;
+	}
+}
