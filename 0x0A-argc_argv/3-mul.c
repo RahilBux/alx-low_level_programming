f97e35380e5081a386a487c0edc3ac25c76f5675
RahilBux <rahilbux@gmail.com>
2023-07-07 23:26:06 +0200

    added

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,69 +1,19 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted
- */
-
-int _atoi(char *s)
-{
-	int i, d, v, l, f, di;
-
-	i = 0;
-	d = 0;
-	v = 0;
-	l = 0;
-	f = 0;
-	di = 0;
-
-	while (s[l] != '\0')
-	{
-		l++;
-	}
-	while (i < l && f == 0)
-	{
-		if (s[i] == '-')
-		{
-			++d;
-		}
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			di = s[i] - '0';
-			if (d % 2)
-			{
-				di = -di;
-			}
-			v = v * 10 + di;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-			{
-				break;
-			}
-			f = 0;
-		}
-		i++;
-	}
-	if (f == 0)
-	{
-		return (0);
-	}
-	return (v);
-}
-
-/**
- * main - multiplies two numbers in argument
+ * main - converts string to an integer and mulitply two numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 if Success and 1 if there are less than or more than 2 arguments
+ * Return: 0(Success) and 1 if error
  */
 
 int main(int argc, char *argv[])
 {
-	int sum1;
-	int sum2;
+	int num1;
+	int num2;
 	int result;
 
 	if (argc != 3)
@@ -72,9 +22,9 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	sum1 = _atoi(argv[1]);
-	sum2 = _atoi(argv[2]);
-	result = sum1 * sum2;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
 
 	printf("%d\n", result);
 	return (0);
