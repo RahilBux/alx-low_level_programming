f71d3293ea87e5940cca7bda22df127718641f92
RahilBux <rahilbux@gmail.com>
2023-07-13 16:49:24 +0200

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - creates a space in memory and sets it to 0
+ * @nmemb: number of elements
+ * @size: size in bytes of each element
+ * Return: void pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+	{
+		ptr[i] = 0;
+	}
+	return (ptr);
+}
