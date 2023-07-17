a765a819f13d5a4785cc1a3aeeab3817c413afc5
RahilBux <rahilbux@gmail.com>
2023-07-17 16:43:24 +0200

    added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef dog_h
 #define dog_h
 
+/**
+ * struct dog - structure for a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Description: struc for a dog
+ */
+
 struct dog
 {
 	char *name;
