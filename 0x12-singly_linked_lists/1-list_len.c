f80f09322b73b773014a8164616f0d02bf7ad9cb
RahilBux <rahilbux@gmail.com>
2023-07-27 19:01:01 +0200

    added task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns length of linked list
+ * @h: pointer to the struct
+ *
+ * Return: length of the list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t s = 0;
+
+	while(h)
+	{
+		s++;
+		h = h->next;
+	}
+	return (s);
+}
