8f038111ba2c91b38315b54f61dd91f38591e09e
RahilBux <rahilbux@gmail.com>
2023-08-03 23:46:43 +0200

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the given number in binary
+ * @n: number to print in binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i, num = 0;
+	unsigned long int c;
+
+	for (i = 63; i >= 0; i--)
+	{
+		c = n >> i;
+
+		if (c & 1)
+		{
+			_putchar('1');
+			num++;
+		}
+		else if (num != 0)
+		{
+			_putchar('0')
+		}
+	}
+	if (num == 0)
+	{
+		_putchar('0');
+	}
+}
