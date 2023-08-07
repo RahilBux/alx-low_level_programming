5fb46967791ef79e1b0297f368a67e914f691517
RahilBux <rahilbux@gmail.com>
2023-08-07 18:26:23 +0200

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: Name of file
+ * @text_content: string to write to the file
+ *
+ * Return: 1 or -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, wr, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[i] != NULL)
+			i++;
+	}
+
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	wr = write(file, text_content, i);
+
+	if (file == -1 || wr == -1)
+		return (-1);
+
+	close(file);
+
+	return (1);
+}
+
