7b248b064c4d6b10e89c1adc1e647dcf8cdfc8a8
RahilBux <rahilbux@gmail.com>
2023-08-07 19:04:25 +0200

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	op = open(filename, O_WRONLY | O_APPEND);
-	wr = write(open, text_content, i);
+	wr = write(op, text_content, i);
 
 	if (op == -1 || wr == -1)
 		return (-1);
